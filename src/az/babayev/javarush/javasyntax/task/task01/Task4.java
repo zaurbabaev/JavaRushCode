091ede79be4fb3c50716a62492300d65edeb2735
Zaur <zaurik87@gmail.com>
2023-11-21 23:32:23 +0400

    task4

---
+++ src/az/babayev/javarush/javasyntax/task/task01/Task4.java
@@ -0,0 +1,11 @@
+package az.babayev.javarush.javasyntax.task.task01;
+
+/*
+Сейчас 3126 год. Мой друг родился 8 лет назад.
+Написать программу, выводящую на экран год рождения моего друга.
+ */
+public class Task4 {
+    public static void main(String[] args) {
+        System.out.println(3126 - 8);
+    }
+}
