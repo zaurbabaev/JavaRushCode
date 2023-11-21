27cfa8e55b6ac8fc880e64b7cdeeb8cc94ab52e5
Zaur <zaurik87@gmail.com>
2023-11-21 23:34:16 +0400

    Task5

---
+++ src/az/babayev/javarush/javasyntax/task/task01/Task5.java
@@ -0,0 +1,16 @@
+package az.babayev.javarush.javasyntax.task.task01;
+
+/*
+Напиши программу, которая в методе main объявляет такие переменные:
+name типа String, age типа int и city типа String.
+
+Примечание: "объявить переменную" - значит то же, что и "создать переменную".
+ */
+public class Task5 {
+    public static void main(String[] args) {
+        String name;
+        int age;
+        String city;
+        
+    }
+}
