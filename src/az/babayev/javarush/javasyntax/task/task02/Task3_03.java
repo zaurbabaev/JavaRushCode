f78a9f3285146c608fb2af04d2a922448ee7a259
Zaur <zaurik87@gmail.com>
2023-11-22 16:21:00 +0400

    Task3_03

---
+++ src/az/babayev/javarush/javasyntax/task/task02/Task3_03.java
@@ -0,0 +1,11 @@
+package az.babayev.javarush.javasyntax.task.task02;
+
+/*
+Напиши программу, которая выводит на экран надпись:
+"Будешь плохо кодить, придет Java и съест твою память".
+ */
+public class Task3_03 {
+    public static void main(String[] args) {
+        System.out.println("Будешь плохо кодить, придет Java и съест твою память");
+    }
+}
