4e3427033f3bf38817dfb9d24c8b5ee48aa17b89
Zaur <zaurik87@gmail.com>
2023-11-22 16:42:32 +0400

    Task5_03

---
+++ src/az/babayev/javarush/javasyntax/task/task02/Task5_02.java
@@ -0,0 +1,23 @@
+package az.babayev.javarush.javasyntax.task.task02;
+
+/*
+Создай 10 переменных типа Cat и 8 объектов типа Cat.
+ */
+public class Task5_02 {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+        Cat cat3 = new Cat();
+        Cat cat4 = new Cat();
+        Cat cat5 = new Cat();
+        Cat cat6 = new Cat();
+        Cat cat7 = new Cat();
+        Cat cat8 = new Cat();
+        Cat cat9;
+        Cat cat10;
+    }
+
+    public static class Cat {
+
+    }
+}
---
+++ src/az/babayev/javarush/javasyntax/task/task02/Task5_03.java
@@ -0,0 +1,36 @@
+package az.babayev.javarush.javasyntax.task.task02;
+
+/*
+Создай объект типа Cat (кот), объект типа Dog (собака), объект типа Fish (рыбка)
+и объект типа Woman.Присвой каждому животному владельца (owner).
+ */
+public class Task5_03 {
+    public static void main(String[] args) {
+        Woman woman = new Woman();
+        Cat cat = new Cat();
+        cat.owner = woman;
+
+        Dog dog = new Dog();
+        dog.owner = woman;
+
+        Fish fish = new Fish();
+        fish.owner = woman;
+    }
+
+    public static class Woman {
+    }
+
+    public static class Cat {
+        Woman owner;
+    }
+
+    public static class Dog {
+        Woman owner;
+    }
+
+    public static class Fish {
+        Woman owner;
+    }
+
+
+}
