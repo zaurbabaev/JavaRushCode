7fc98f70a5e6447b4f0c4ebaa74c5ccc550a1f9a
Zaur <zaurik87@gmail.com>
2023-11-22 16:13:19 +0400

    Task3_01

---
+++ src/az/babayev/javarush/javasyntax/task/task02/Task3_01.java
@@ -0,0 +1,17 @@
+package az.babayev.javarush.javasyntax.task.task02;
+
+/*
+Создать объект типа Cat 2 раза.
+Сохрани каждый экземпляр в свою переменную.
+Имена переменных должны быть разные.
+ */
+public class Task3_01 {
+    public static void main(String[] args) {
+        Cat cat1 = new Cat();
+        Cat cat2 = new Cat();
+    }
+
+    public static class Cat {
+
+    }
+}
