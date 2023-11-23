e641a2b3a97649f8b9d8eb2ae7332ab88efb01a7
Zaur <zaurik87@gmail.com>
2023-11-23 16:37:05 +0400

    Task12_01

---
+++ src/az/babayev/javarush/javasyntax/task/task03/Task12_01.java
@@ -0,0 +1,13 @@
+package az.babayev.javarush.javasyntax.task.task03;
+/*
+Вывести на экран десять раз надпись
+"Я хочу большую зарплату, и для этого изучаю Java"
+ */
+public class Task12_01 {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Я хочу большую зарплату, и для этого изучаю Java");
+        }
+    }
+
+}
