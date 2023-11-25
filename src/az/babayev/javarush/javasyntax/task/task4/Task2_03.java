d3373c6405be646c3f2242fd06cb126760ce7286
Zaur <zaurik87@gmail.com>
2023-11-25 11:27:37 +0400

    Task2_03

---
+++ src/az/babayev/javarush/javasyntax/task/task4/Task2_03.java
@@ -0,0 +1,23 @@
+package az.babayev.javarush.javasyntax.task.task4;
+
+/*
+Напиши метод setCatsCount.
+Метод должен устанавливать количество котов (catsCount).
+ */
+public class Task2_03 {
+
+    public static class Cat {
+
+        private static int catsCount = 0;
+
+        private void setCatsCount(int count) {
+            catsCount = count;
+        }
+
+        public static void main(String[] args) {
+            Cat cat = new Cat();
+            cat.setCatsCount(3);
+            System.out.println(catsCount);
+        }
+    }
+}
