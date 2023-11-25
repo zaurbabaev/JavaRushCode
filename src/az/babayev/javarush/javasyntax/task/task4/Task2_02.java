3fcbe4882be66ed32ba31b3b9d4dfa4cc364df07
Zaur <zaurik87@gmail.com>
2023-11-25 11:21:31 +0400

    Task2_02

---
+++ src/az/babayev/javarush/javasyntax/task/task4/Task2_02.java
@@ -0,0 +1,27 @@
+package az.babayev.javarush.javasyntax.task.task4;
+
+/*
+Напиши код в методе addNewCat, чтобы при его вызове количество
+котов увеличивалось на 1.
+За количество котов отвечает переменная catsCount.
+ */
+public class Task2_02 {
+
+    public static class Cat {
+        private static int catsCount=0;
+
+        public static void addNewCat() {
+            catsCount++;
+        }
+
+        public static void main(String[] args) {
+            Cat cat = new Cat();
+            addNewCat();
+            System.out.println(catsCount);
+            Cat cat2 = new Cat();
+            addNewCat();
+            System.out.println(catsCount);
+
+        }
+    }
+}
