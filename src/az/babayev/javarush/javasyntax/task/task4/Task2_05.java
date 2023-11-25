107fa079dbe5eca47eefd62732746b12f443e8f2
Zaur <zaurik87@gmail.com>
2023-11-25 11:43:42 +0400

    Task02_5

---
+++ src/az/babayev/javarush/javasyntax/task/task4/Task2_05.java
@@ -0,0 +1,23 @@
+package az.babayev.javarush.javasyntax.task.task4;
+
+/*
+Написать код, чтобы правильно считалось количество созданных котов (count)
+и на экран выдавалось правильное их количество.
+ */
+public class Task2_05 {
+    public static void main(String[] args) {
+       Cat cat=new Cat();
+       Cat cat2=new Cat();
+       Cat cat3=new Cat();
+
+
+    }
+
+    public static class Cat {
+        public static int count = 0;
+
+        Cat() {
+            System.out.println(++count);
+        }
+    }
+}
