4b817ddf7ace601294e4014b9b325ac0a4f61e35
Zaur <zaurik87@gmail.com>
2023-11-25 16:14:06 +0400

    Task6_05

--- src/az/babayev/javarush/javasyntax/task/task04/Task6_05.java
+++ src/az/babayev/javarush/javasyntax/task/task04/Task6_05.java
@@ -1,4 +1,22 @@
 package az.babayev.javarush.javasyntax.task.task04;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Ввести с клавиатуры имя и возраст.
+Если возраст меньше 18 вывести надпись "Подрасти еще".
+ */
 public class Task6_05 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        String name = bufferedReader.readLine();
+        String string = bufferedReader.readLine();
+        int age = Integer.parseInt(string);
+        if (age < 18) {
+            System.out.println("Подрасти еще");
+        }
+
+    }
 }
