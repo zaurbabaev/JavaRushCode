cb0a4e634203f9540a0ae33922ca9e55deeb0900
Zaur <zaurik87@gmail.com>
2023-11-25 11:36:47 +0400

    Task2_04

---
+++ src/az/babayev/javarush/javasyntax/task/task4/Task2_04.java
@@ -0,0 +1,24 @@
+package az.babayev.javarush.javasyntax.task.task4;
+
+/*
+Допишите код метода setName, чтобы с его помощью установить значение
+private String fullName равное значению локальной переменной String fullName.
+ */
+public class Task2_04 {
+
+    public static class Cat {
+        private String fullName;
+
+        public void setName(String firstName, String lastName) {
+            String fullName = firstName + " " + lastName;
+            this.fullName = fullName;
+        }
+
+        public static void main(String[] args) {
+            Cat cat =new Cat();
+            cat.setName("Barsik","Barsik");
+            System.out.println(cat.fullName);
+
+        }
+    }
+}
