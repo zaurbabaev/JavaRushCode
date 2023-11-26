9ab4de3c6575897c0f0e8b22fe3ad237665ce752
Zaur <zaurik87@gmail.com>
2023-11-26 18:35:28 +0400

    Task5_06

---
+++ src/az/babayev/javarush/javasyntax/task/task5/Task5_06.java
@@ -0,0 +1,14 @@
+package az.babayev.javarush.javasyntax.task.task5;
+/*
+Создать class Person. У человека должно быть имя String name,
+возраст int age, адрес String address, пол char sex.
+ */
+public class Task5_06 {
+    public static class Person{
+        private String name;
+        private int age;
+        private String address;
+
+        private char sex;
+    }
+}
