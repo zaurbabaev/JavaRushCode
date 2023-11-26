c2da8f5c50a0b88752fcaea70554c03025c14329
Zaur <zaurik87@gmail.com>
2023-11-26 18:13:29 +0400

    Task5_03

---
+++ src/az/babayev/javarush/javasyntax/task/task5/Task5_03.java
@@ -0,0 +1,28 @@
+package az.babayev.javarush.javasyntax.task.task5;
+/*
+Создать class Dog. У собаки должна быть кличка String name и возраст int age.
+Создай геттеры и сеттеры для всех переменных класса Dog.
+ */
+public class Task5_03
+{
+    public static class Dog{
+        String name;
+        int age;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+    }
+}
