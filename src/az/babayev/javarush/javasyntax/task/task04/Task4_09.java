93c94e0dc13f62d85a9fd78238222eb278816df0
Zaur <zaurik87@gmail.com>
2023-12-02 11:50:56 +0400

    Svetofor

---
+++ src/az/babayev/javarush/javasyntax/task/task04/Task4_09.java
@@ -0,0 +1,46 @@
+package az.babayev.javarush.javasyntax.task.task04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Работа светофора для пешеходов запрограммирована следующим образом:
+в начале каждого часа в течение трех минут горит зелёный сигнал,
+затем в течение одной минуты - жёлтый,
+а потом в течение одной минуты - красный,
+затем опять зелёный горит три минуты и т. д.
+Ввести с клавиатуры вещественное число t, означающее время в минутах, прошедшее с начала очередного часа.
+Определить, сигнал какого цвета горит для пешеходов в этот момент.
+
+Результат вывести на экран в следующем виде:
+"зелёный" - если горит зелёный цвет,
+"жёлтый" - если горит жёлтый цвет,
+"красный" - если горит красный цвет.
+
+Пример для числа 2.5:
+зелёный
+
+Пример для числа 3:
+жёлтый
+
+Пример для числа 4:
+красный
+
+Пример для числа 5:
+зелёный
+ */
+public class Task4_09 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        double t = Double.parseDouble(reader.readLine());
+        double result = t % 5;
+        if (result<3) {
+            System.out.println("зелёный");
+        } else if (result>=3&&result<4) {
+            System.out.println("жёлтый");
+        } else {
+            System.out.println("красный");
+        }
+    }
+}
