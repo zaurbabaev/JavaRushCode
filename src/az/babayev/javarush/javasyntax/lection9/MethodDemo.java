e7318559b5cd3b146a0f41a6dd6b6aa7bd7464e1
Zaur <zaurik87@gmail.com>
2023-12-02 17:04:08 +0400

    Methods

---
+++ src/az/babayev/javarush/javasyntax/lection9/MethodDemo.java
@@ -0,0 +1,32 @@
+package az.babayev.javarush.javasyntax.lection9;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class MethodDemo {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Input count grades:");
+        int count = Integer.parseInt(reader.readLine());
+        int[] grades = new int[count];
+        for (int i = 0; i < grades.length; i++) {
+            System.out.println("Input grades:[" + i + "]");
+            grades[i] = Integer.parseInt(reader.readLine());
+        }
+        printGrades(grades, "[", ".", "]");
+    }
+
+    private static void printGrades(int[] grades, String leftBrackets, String delimiter, String rightBrackets) {
+        System.out.print(leftBrackets);
+        for (int i = 0; i < grades.length; i++) {
+            System.out.print(grades[i]);
+            if (i < grades.length - 1) {
+                System.out.print(delimiter);
+            }
+        }
+        System.out.println(rightBrackets);
+    }
+
+
+}
