0beabde4bd044fb7f2f979d9660639c5eba63375
Zaur <zaurik87@gmail.com>
2023-12-02 12:13:28 +0400

    3 or 2 Same numbers

---
+++ src/az/babayev/javarush/javasyntax/task/task04/Task4_10.java
@@ -0,0 +1,43 @@
+package az.babayev.javarush.javasyntax.task.task04;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/*
+Ввести с клавиатуры три целых числа. Определить, имеется ли среди них хотя бы одна пара равных между собой чисел.
+Если такая пара существует, вывести на экран числа через пробел.
+Если все три числа равны между собой, то вывести все три.
+
+Примеры:
+а) при вводе чисел
+1
+2
+2
+получим вывод
+2 2
+
+б) при вводе чисел
+2
+2
+2
+получим вывод
+2 2 2
+ */
+public class Task4_10 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        int a = Integer.parseInt(reader.readLine());
+        int b = Integer.parseInt(reader.readLine());
+        int c = Integer.parseInt(reader.readLine());
+        if (a == b && a == c) {
+            System.out.println(a + " " + b + " " + c);
+        } else if (a == b) {
+            System.out.println(a + " " + b);
+        } else if (a == c) {
+            System.out.println(a + " " + c);
+        } else if (b == c) {
+            System.out.println(b + " " + c);
+        }
+    }
+}
