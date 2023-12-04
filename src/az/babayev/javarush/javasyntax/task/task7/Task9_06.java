3ed188d7a3b52bf0620b0df50e7776dd43221541
Zaur <zaurik87@gmail.com>
2023-12-04 19:15:49 +0400

    Task9_06
    список упорядоченным по возрастанию длины строки

---
+++ src/az/babayev/javarush/javasyntax/task/task7/Task9_06.java
@@ -0,0 +1,28 @@
+package az.babayev.javarush.javasyntax.task.task7;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+/*
+1. Введи с клавиатуры 10 слов в список строк.
+2. Определить, является ли список упорядоченным по возрастанию длины строки.
+3. В случае отрицательного ответа вывести на экран индекс первого элемента,
+нарушающего такую упорядоченность.
+ */
+public class Task9_06 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            list.add(reader.readLine());
+        }
+        for (int i = 1; i < list.size(); i++) {
+            if (list.get(i - 1).length() > list.get(i).length()) {
+                System.out.println(i);
+                break;
+            }
+        }
+    }
+}
