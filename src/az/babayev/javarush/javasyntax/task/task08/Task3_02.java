ac3cbbc5ca6f43d091d68e558235ebc01f97857b
Zaur <zaurik87@gmail.com>
2023-12-05 23:13:50 +0400

    HashSet()

---
+++ src/az/babayev/javarush/javasyntax/task/task08/Task3_02.java
@@ -0,0 +1,37 @@
+package az.babayev.javarush.javasyntax.task.task08;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/*
+Создать коллекцию Set (реализация HashSet) с типом элементов String.
+Добавить в неё следующие строки:
+арбуз
+банан
+вишня
+груша
+дыня
+ежевика
+женьшень
+земляника
+ирис
+картофель
+ */
+public class Task3_02 {
+    public static void main(String[] args) {
+        Set<String> set = new HashSet<>();
+        set.add("арбуз");
+        set.add("банан");
+        set.add("вишня");
+        set.add("груша");
+        set.add("дыня");
+        set.add("ежевика");
+        set.add("женьшень");
+        set.add("земляника");
+        set.add("ирис");
+        set.add("картофель");
+        for (String s : set) {
+            System.out.println(s);
+        }
+    }
+}
