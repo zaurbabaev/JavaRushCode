d8890286cefcf61a4d7942326f30f269bfa3a9fd
Zaur <zaurik87@gmail.com>
2023-12-16 19:29:02 +0400

    Task11_03

---
+++ src/az/babayev/javarush/javasyntax/task/task08/Task11_03.java
@@ -0,0 +1,39 @@
+package az.babayev.javarush.javasyntax.task.task08;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+1. Создать словарь Map (<String, String>) и добавить туда 10 человек в виде "Фамилия"-"Имя".
+2. Пусть среди этих 10 человек есть люди с одинаковыми именами.
+3. Пусть среди этих 10 человек есть люди с одинаковыми фамилиями.
+4. Вывести содержимое Map на экран.
+ */
+public class Task11_03 {
+    public static void main(String[] args) {
+        Map<String, String> map = createPeopleMap();
+        printPeopleMap(map);
+    }
+
+
+    public static Map<String, String> createPeopleMap() {
+        Map<String, String> map = new HashMap<>();
+        map.put("Смирнов", "Сильвестр");
+        map.put("Иванов", "Дмитрий");
+        map.put("Кузнецов", "Арнольд");
+        map.put("Соколов", "Роман");
+        map.put("Попов", "Максим");
+        map.put("Лебедев", "Ярослав");
+        map.put("Новиков", "Александр");
+        map.put("Козлов", "Михаил");
+        map.put("Морозов", "Илья");
+        map.put("Новиков", "Алексей");
+        return map;
+    }
+
+    public static void printPeopleMap(Map<String, String> map) {
+        for (Map.Entry<String, String> s : map.entrySet()) {
+            System.out.println(s.getKey() + " " + s.getValue());
+        }
+    }
+}
