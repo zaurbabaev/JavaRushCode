6f175483310d728f5a14bb2e0a6601fcad59a1b2
Zaur <zaurik87@gmail.com>
2023-12-29 20:55:13 +0400

    Task0204
    Increment

---
+++ src/az/babayev/javarush/javasyntax/task/task02/Increment.java
@@ -0,0 +1,15 @@
+package az.babayev.javarush.javasyntax.task.task02;
+
+/*
+Используя только оператор инкремента, измени значение в переменной six,
+ чтобы на экран вывелась цифра 9.
+ */
+public class Increment {
+    public static void main(String[] args) {
+        int six = 6;
+        six ++;
+        six ++;
+        six ++;
+        System.out.println(six);
+    }
+}
