cc1ee54763b85df3b361aaebe63a1fdb5618755e
Zaur <zaurik87@gmail.com>
2024-01-01 13:33:57 +0400

    Task0411
    Even numbers

---
+++ src/az/babayev/javarush/javasyntax/task/task04/EvenNumbers.java
@@ -0,0 +1,14 @@
+package az.babayev.javarush.javasyntax.task.task04;
+/*
+Напиши программу, в которой с помощью цикла for на экран будут выведены чётные числа от 1 до 15.
+Каждое значение нужно выводить с новой строки.
+ */
+public class EvenNumbers {
+    public static void main(String[] args) {
+        for (int i = 1; i < 15; i++) {
+            if(i%2==0){
+                System.out.println(i);
+            }
+        }
+    }
+}
