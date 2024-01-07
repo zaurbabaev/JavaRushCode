2bf5768c3eb07fe350471868c982fcca4907b664
Zaur <zaurik87@gmail.com>
2024-01-07 20:05:13 +0400

    Task0606
    override methods

---
+++ src/az/babayev/javarush/javasyntax/task/task7/UniversalMethods.java
@@ -0,0 +1,47 @@
+package az.babayev.javarush.javasyntax.task.task7;
+/*
+Неплохо было бы иметь один метод для решения различных задач.
+У тебя есть возможность написать такой.
+Создай еще 9 методов universalMethod(). В сумме их должно получиться 10.
+Параметры, которые они должны принимать, придумай сам.
+ */
+public class UniversalMethods {
+    public static void main(String[] args) {
+
+    }
+
+    public static void universalMethod(){
+
+    }
+
+    public static void universalMethod(String s){
+
+    }
+    public static void universalMethod(int i){
+
+    }
+    public static void universalMethod(double d){
+
+    }
+    public static void universalMethod(short s){
+
+    }
+    public static void universalMethod(byte b){
+
+    }
+    public static void universalMethod(char c){
+
+    }
+    public static void universalMethod(float f){
+
+    }
+    public static void universalMethod(long l){
+
+    }
+    public static void universalMethod(boolean b){
+
+    }
+
+
+
+}
