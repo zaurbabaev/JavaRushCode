04df29416a4e45cba418d0b74ecce0300d6b8543
Zaur <zaurik87@gmail.com>
2024-01-07 21:56:19 +0400

    Task0615
    Earth

---
+++ src/az/babayev/javarush/javasyntax/task/task7/Earth.java
@@ -0,0 +1,14 @@
+package az.babayev.javarush.javasyntax.task.task7;
+
+/*
+Перед тобой класс Earth - сборник переменных, описывающих определенные характеристики планеты Земля.
+Сделай из переменных класса Earth глобальные константы. Не забудь переименовать переменные,
+чтобы они отвечали стилю написания глобальных переменных.
+ */
+public class Earth {
+    public static  final String NAME = "Земля";
+    public static final double SQUARE = 510_100_000;
+    public static final long POPULATION = 7_594_000_000L;
+    public static final long EQUATOR_LENGTH = 40_075_696;
+
+}
