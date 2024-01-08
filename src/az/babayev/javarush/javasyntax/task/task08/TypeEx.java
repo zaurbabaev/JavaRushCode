53a036b2a527a8a1b5bb9c86abf2c25d81d1ce09
Zaur <zaurik87@gmail.com>
2024-01-08 17:34:00 +0400

    Task0701
    Data type

---
+++ src/az/babayev/javarush/javasyntax/task/task08/TypeEx.java
@@ -0,0 +1,17 @@
+package az.babayev.javarush.javasyntax.task.task08;
+
+/*
+Как тебе уже известно, всего в языке Java 8 примитивных типов.
+Создай по одной переменной каждого примитивного типа в классе TypeEx.
+Переменные не должны находиться в каком-либо методе
+ */
+public class TypeEx {
+    byte b = 1;
+    short s = 8;
+    int i = 12;
+    long l = 10L;
+    float f = 10.3f;
+    double d = 30.3;
+    char c = 'A';
+    boolean bool = true;
+}
