4a0dab5c710144a6d5cad7e65c354c4b7198f0c2
Zaur <zaurik87@gmail.com>
2024-01-16 15:02:51 +0400

    Task1106
    Lonely cars

---
+++ src/az/babayev/javarush/javasyntax/task/task12/lonely_cars/Car.java
@@ -0,0 +1,32 @@
+package az.babayev.javarush.javasyntax.task.task12.lonely_cars;
+
+/*
+В далеком-далеком прошлом все машины называли одинаково. Но вот в один прекрасный день
+директору завода по производству автомобилей надоело однообразие,
+и он выдал распоряжение: давать разные названия моделям авто.
+Итак, как ты уже догадался, твоя задача — помочь главному инженеру исправить
+программу по производству машин так, чтобы каждому объекту-машине можно было
+присвоить уникальное имя.
+ */
+public class Car {
+    private String modelName;
+    private int speed;
+
+    public String getModelName() {
+        return modelName;
+    }
+
+    public void setModelName(String modelName) {
+        this.modelName = modelName;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+
+}
