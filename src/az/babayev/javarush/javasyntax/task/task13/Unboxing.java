469a27b26df1a311bad94ece48333a0e82839ce6
Zaur <zaurik87@gmail.com>
2024-01-17 19:18:07 +0400

    Task1202
    Unboxing

---
+++ src/az/babayev/javarush/javasyntax/task/task13/Unboxing.java
@@ -0,0 +1,34 @@
+package az.babayev.javarush.javasyntax.task.task13;
+
+/*
+В классе Solution представлены 8 объектов-оберток примитивных типов с именами
+вида типValueBox.
+Для каждого из этих объектов-оберток необходимо создать переменные соответствующего
+примитивного типа и присвоить им имена вида типValue.
+Для создания переменной используй метод объекта-обертки типValueBox.примитивValue().
+
+Пример переменной примитивного типа: int intValue = ....;
+ */
+public class Unboxing {
+    Byte byteValueBox;
+    Short shortValueBox;
+    Integer integerValueBox;
+    Long longValueBox;
+
+    Float floatValueBox;
+    Double doubleValueBox;
+
+    Character characterValueBox;
+    Boolean booleanValueBox;
+
+    byte byteValue = byteValueBox.byteValue();
+    short shortValue = shortValueBox.shortValue();
+    int intValue = integerValueBox.intValue();
+    long longValue = longValueBox.longValue();
+
+    float floatValue = floatValueBox.floatValue();
+    double doubleValue = doubleValueBox.doubleValue();
+
+    char charValue = characterValueBox.charValue();
+    boolean booleanValue = booleanValueBox.booleanValue();
+}
