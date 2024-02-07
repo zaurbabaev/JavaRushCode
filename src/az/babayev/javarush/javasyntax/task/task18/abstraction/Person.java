5ba094fc0131c36dcd642a1048b9476ed77fc26c
Zaur <zaurik87@gmail.com>
2024-02-07 17:30:15 +0400

    Task1710

---
+++ src/az/babayev/javarush/javasyntax/task/task18/abstraction/Person.java
@@ -0,0 +1,29 @@
+package az.babayev.javarush.javasyntax.task.task18.abstraction;
+
+/*
+Класс Person объявлен как абстрактный. Это значит, что мы не можем создавать его
+экземпляры.
+Сделай из данного класса обычный, допиши недостающую логику, которая отвечает
+общепринятым подходам к инкапсуляции:
+геттеры должны возвращать, а сеттеры — устанавливать значения соответствующим полям.
+ */
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
