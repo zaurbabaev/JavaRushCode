062df5d99203967eaba2ad6f0326c30f4d0215f6
Zaur <zaurik87@gmail.com>
2024-02-07 18:36:08 +0400

    Task1717

---
+++ src/az/babayev/javarush/javasyntax/task/task18/interfaceex/task1717/Raven.java
@@ -0,0 +1,8 @@
+package az.babayev.javarush.javasyntax.task.task18.interfaceex.task1717;
+
+public class Raven implements Flyable{
+    @Override
+    public int getMaxSpeed() {
+        return 48;
+    }
+}
