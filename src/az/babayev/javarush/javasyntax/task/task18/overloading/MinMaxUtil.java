ebd709b830649d3fa7db57355bf8f0e835d5ef8e
Zaur <zaurik87@gmail.com>
2024-02-07 17:11:16 +0400

    Task1708

---
+++ src/az/babayev/javarush/javasyntax/task/task18/overloading/MinMaxUtil.java
@@ -0,0 +1,60 @@
+package az.babayev.javarush.javasyntax.task.task18.overloading;
+
+/*
+В этой задаче тебе предстоит создать универсальный инструмент для поиска
+минимальных и максимальных чисел.
+В классе MinMaxUtil создай публичные статические методы min() и max(),
+которые в качестве аргументов принимают целочисленные (тип int) значения и
+возвращают минимальное и максимальное из них соответственно.
+
+Каждый из методов должен быть перегружен так, чтобы была возможность вызвать
+метод с двумя, тремя, четырьмя и пятью параметрами.
+В общем, необходимо создать 8 методов, — 4 для min() и 4 для max().
+Все аргументы методов должны быть типа int.
+ */
+public class MinMaxUtil {
+    public static void main(String[] args) {
+        System.out.println(min(12, 2));
+        System.out.println(min(12, 2, 5));
+        System.out.println(min(12, 0, -9, 10));
+        System.out.println(min(12, 3, 45, -4, 8));
+        System.out.println(max(12, 24));
+        System.out.println(max(12, 2, 52));
+        System.out.println(max(12, 0, -9, 130));
+        System.out.println(max(12, 3, 45, -4, 80));
+    }
+
+    public static int min(int a, int b) {
+        return a < b ? a : b;
+    }
+
+    public static int min(int a, int b, int c) {
+        return min(a, b) < c ? min(a, b) : c;
+    }
+
+    public static int min(int a, int b, int c, int d) {
+        return min(a, b, c) < d ? min(a, b, c) : d;
+    }
+
+    public static int min(int a, int b, int c, int d, int e) {
+        return min(a, b, c, d) < e ? min(a, b, c, d) : e;
+    }
+
+    public static int max(int a, int b) {
+        return a > b ? a : b;
+    }
+
+    public static int max(int a, int b, int c) {
+
+        return max(a, b) > c ? max(a, b) : c;
+    }
+
+    public static int max(int a, int b, int c, int d) {
+        return max(a, b, c) > d ? max(a, b, c) : d;
+    }
+
+    public static int max(int a, int b, int c, int d, int e) {
+        return max(a, b, c, d) > e ? max(a, b, c, d) : e;
+    }
+
+}
