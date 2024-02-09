042d4883069854134b452642ce5d96bd8d78fbaa
Zaur <zaurik87@gmail.com>
2024-02-09 22:11:03 +0400

    Task1801

---
+++ src/az/babayev/javarush/javasyntax/task/task19/task1801/Car.java
@@ -0,0 +1,8 @@
+package az.babayev.javarush.javasyntax.task.task19.task1801;
+
+public class Car implements Runnable {
+    @Override
+    public void run() {
+        System.out.println("Car is running!");
+    }
+}
---
+++ src/az/babayev/javarush/javasyntax/task/task19/task1801/Runnable.java
@@ -0,0 +1,5 @@
+package az.babayev.javarush.javasyntax.task.task19.task1801;
+
+public interface Runnable {
+    void run();
+}
