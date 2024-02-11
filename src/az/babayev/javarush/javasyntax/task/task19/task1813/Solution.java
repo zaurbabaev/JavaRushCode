62a0b76927254f9a7c8e35a45c1a0461342a0db4
Zaur <zaurik87@gmail.com>
2024-02-11 22:44:51 +0400

    Task1813

---
+++ src/az/babayev/javarush/javasyntax/task/task19/task1813/Solution.java
@@ -0,0 +1,42 @@
+package az.babayev.javarush.javasyntax.task.task19.task1813;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Stream;
+
+/*
+Современный мир отказывается от двигателей внутреннего сгорания, и эра электрокаров
+не за горами. Нужно, чтобы наша программа могла отфильтровать электрокары от всех
+остальных автомобилей.
+Для этих целей есть метод onlyElectricCars(ArrayList<Car>), который фильтрует
+список автомобилей, получаемых в качестве входящего параметра. Этот метод
+возвращает поток отфильтрованных данных, а именно — поток автомобилей,
+у которых поле isElectric равно true. В этом тебе поможет метод filter()
+объекта типа Stream<Car>.
+
+Метод main() не принимает участие в тестировании.
+ */
+public class Solution {
+    public static void main(String[] args) {
+        var cars = new ArrayList<Car>(List.of(
+                new Car("Range Rover", false),
+                new Car("Model S", true),
+                new Car("Navigator", false),
+                new Car("Model 3", true),
+                new Car("Camaro", false),
+                new Car("Escalade", false),
+                new Car("Mustang", false),
+                new Car("Model X", true),
+                new Car("X5", false),
+                new Car("Model Y", true)
+        ));
+
+        var carStream = onlyElectricCars(cars);
+        carStream.forEach(System.out::println);
+
+    }
+
+    public static Stream<Car> onlyElectricCars(ArrayList<Car> cars) {
+        return cars.stream().filter(Car::isElectric);
+    }
+}
