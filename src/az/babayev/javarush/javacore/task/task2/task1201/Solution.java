7103147c9f1e4d3e7e84f1735a89e2cc01d23083
Zaur <zaurik87@gmail.com>
2024-02-16 14:52:55 +0400

    Task1201

---
+++ src/az/babayev/javarush/javacore/task/task2/task1201/Solution.java
@@ -0,0 +1,28 @@
+package az.babayev.javarush.javacore.task.task2.task1201;
+
+/*
+Переопредели метод getName в классе Whale(Кит), чтобы программа выдавала:
+
+Я не корова, Я - кит. */
+public class Solution {
+    public static void main(String[] args) {
+
+        Cow cow = new Whale();
+        System.out.println(cow.getName());
+    }
+
+    public static class Cow {
+
+        public String getName() {
+            return "Я – корова";
+        }
+    }
+
+    public static class Whale extends Cow {
+
+        @Override
+        public String getName() {
+            return "Я не корова, Я - кит.";
+        }
+    }
+}
