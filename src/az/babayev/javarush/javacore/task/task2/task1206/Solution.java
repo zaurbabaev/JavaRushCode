d11870eb1fcd4ce23e70a0981363ff2c63ec3943
Zaur <zaurik87@gmail.com>
2024-02-16 16:38:56 +0400

    Task1206

---
+++ src/az/babayev/javarush/javacore/task/task2/task1206/Solution.java
@@ -0,0 +1,18 @@
+package az.babayev.javarush.javacore.task.task2.task1206;
+
+/*
+Напиши два метода: print(int) и print(String).
+ */
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    public int print(int i) {
+        return i;
+    }
+
+    public String print(String s) {
+        return s;
+    }
+}
