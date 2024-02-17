181944fb23471f40b0f428f9fc7de29b4cd1887f
Zaur <zaurik87@gmail.com>
2024-02-17 20:20:28 +0400

    Task1221

---
+++ src/az/babayev/javarush/javacore/task/task2/task1221/Cat.java
@@ -0,0 +1,10 @@
+package az.babayev.javarush.javacore.task.task2.task1221;
+
+public class Cat extends Pet {
+
+	@Override
+	public String getName() {
+		return "Я - кот";
+	}
+
+}
---
+++ src/az/babayev/javarush/javacore/task/task2/task1221/Solution.java
@@ -0,0 +1,10 @@
+package az.babayev.javarush.javacore.task.task2.task1221;
+
+public class Solution {
+	
+	public static void main(String[] args) {
+		Pet pet = new Cat();
+		System.out.println(pet.getName());
+	}
+
+}
