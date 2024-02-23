dcb9be2a62791f108023ae38823f75692e19024f
Zaur <zaurik87@gmail.com>
2024-02-23 21:28:09 +0400

    Task 1507 / Task 1508

---
+++ src/az/babayev/javarush/javacore/task/task5/task1508/Solution.java
@@ -0,0 +1,28 @@
+package az.babayev.javarush.javacore.task.task5.task1508;
+
+public class Solution {
+    public static void main(String[] args) {
+        print(1);
+    }
+
+    public static void print(byte b) {
+        System.out.println("Я буду Java прогером!");
+    }
+
+    public static void print(double d) {
+        System.out.println("Я буду Java прогером!");
+    }
+
+    public static void print(long l) {
+        System.out.println("Я буду Java прогером!");
+    }
+
+    public static void print(float f) {
+        System.out.println("Я буду Java прогером!");
+    }
+
+    public static void print(char c) {
+        System.out.println("Я буду Java прогером!");
+    }
+
+}
