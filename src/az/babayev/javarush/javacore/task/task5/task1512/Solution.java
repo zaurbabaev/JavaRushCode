2618797025fdb071239a4dace02bea17ba5f1a78
Zaur <zaurik87@gmail.com>
2024-02-23 23:33:36 +0400

    Task 1512

---
+++ src/az/babayev/javarush/javacore/task/task5/task1512/Solution.java
@@ -0,0 +1,37 @@
+package az.babayev.javarush.javacore.task.task5.task1512;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        SiamCat simka = new SiamCat("Simka");
+        NakedCat nakedSimka = simka.shave();
+        System.out.println(nakedSimka.getClass().getSimpleName());
+    }
+
+    public static class NakedCat {
+
+    }
+
+    public static class NormalCat extends NakedCat {
+
+        public NormalCat() {
+
+        }
+
+        public NormalCat(String name) {
+            System.out.println("My name is " + name);
+        }
+
+        public NakedCat shave() {
+            return this;
+        }
+
+    }
+
+    public static class SiamCat extends NormalCat {
+
+        public SiamCat(String name) {
+            super(name);
+        }
+    }
+}
