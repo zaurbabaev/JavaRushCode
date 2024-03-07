a79c4f4377b1fcbc6b50306741f3149c3abbbc3f
Zaur <zaurik87@gmail.com>
2024-03-07 22:19:36 +0400

    Task 1818. Stream anyMatch(); LocalDate.now();

---
+++ src/az/babayev/javarush/javasyntax/task/task19/task1818/Product.java
@@ -0,0 +1,7 @@
+package az.babayev.javarush.javasyntax.task.task19.task1818;
+
+import java.time.LocalDate;
+
+public record Product(String name, LocalDate expirationDate) {
+
+}
---
+++ src/az/babayev/javarush/javasyntax/task/task19/task1818/Solution.java
@@ -0,0 +1,30 @@
+package az.babayev.javarush.javasyntax.task.task19.task1818;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.stream.Stream;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        var productStream = Stream.of(
+                new Product("Milk", LocalDate.of(2020, Month.MARCH, 12)),
+                new Product("Cheese", LocalDate.of(2020, Month.MARCH, 31)),
+                new Product("Buckwheat", LocalDate.of(2021, Month.MARCH, 1)),
+                new Product("Rice", LocalDate.of(2022, Month.MAY, 11)),
+                new Product("Wine", LocalDate.of(2023, Month.JANUARY, 10)),
+                new Product("Tea", LocalDate.of(2024, Month.AUGUST, 27)),
+                new Product("Salt", LocalDate.of(2025, Month.DECEMBER, 15))
+        );
+
+        String answer = hasExpiredProduct(productStream) ? "Yes" : "No";
+        System.out.println("Has stream expired products? " + answer);
+    }
+
+    public static boolean hasExpiredProduct(Stream<Product> productStream) {
+        return productStream.anyMatch(product -> product.expirationDate().isBefore(LocalDate.now()));
+    }
+
+}
+
+
