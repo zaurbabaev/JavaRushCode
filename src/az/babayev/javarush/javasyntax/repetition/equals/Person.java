16a490194a6b3a238bb537bc7619c4ffc6002b9b
Zaur <zaurik87@gmail.com>
2024-03-22 22:56:46 +0400

    equals() and hashCode()

---
+++ src/az/babayev/javarush/javasyntax/repetition/equals/Person.java
@@ -0,0 +1,41 @@
+package az.babayev.javarush.javasyntax.repetition.equals;
+
+import java.util.Objects;
+
+public class Person {
+    private String name;
+    private int age;
+
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        return Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        int result = name != null ? name.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
+
+}
