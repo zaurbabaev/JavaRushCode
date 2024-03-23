ba8988a586eb5bc5e746acad716b864e01835fe1
Zaur <zaurik87@gmail.com>
2024-03-23 23:06:53 +0400

    Character example

---
+++ src/az/babayev/javarush/javasyntax/repetition/wraper_type/CharacterExample.java
@@ -0,0 +1,15 @@
+package az.babayev.javarush.javasyntax.repetition.wraper_type;
+
+public class CharacterExample {
+    public static void main(String[] args) {
+        System.out.println(Character.isAlphabetic(65));
+        System.out.println(Character.isDigit('1'));
+        System.out.println(Character.isUpperCase('I'));
+        System.out.println(Character.isLowerCase('I'));
+        System.out.println(Character.isWhitespace('\n'));
+        System.out.println(Character.isSpaceChar('\n'));
+        System.out.println(Character.toLowerCase('I'));
+
+       
+    }
+}
