a57703b0ac780f4071bbc6e5df1b6d3690ecfff5
Zaur <zaurik87@gmail.com>
2024-03-24 23:04:45 +0400

    Bubble sort example

---
+++ src/az/babayev/javarush/javasyntax/repetition/collections/Sort.java
@@ -0,0 +1,27 @@
+package az.babayev.javarush.javasyntax.repetition.collections;
+
+import java.util.Arrays;
+import java.util.Random;
+
+public class Sort {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = random.nextInt(10, 100);
+        }
+        System.out.println(Arrays.toString(array));
+
+        for (int i = array.length - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (array[j] > array[j + 1]) {
+                    int temp = array[j];
+                    array[j] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(array));
+    }
+}
